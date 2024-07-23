da0ed66f0cc700960d98e52caea9410d50b07777
cursedcadaver <collinschacha@hotmail.com>
2024-07-23 19:59:06 +0300

    wtf is this impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-use std::env;
+use std::{env, ops::Index};
 
-const ENCODE_CHARS: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const ENCODE_CHARS: [char; 64] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9', '+', '/',
+];
+
+struct Base64 {
+    char: Vec<char>,
+}
+
+impl Index<u8> for Base64 {
+    type Output = u8;
+    fn index(&self, index: u8) -> &Self::Output {
+        &self.char[index as usize]
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,10 +30,10 @@ fn main() {
 fn encode_base64(bytes: &[u8]) -> String {
     let mut encoded = String::new();
     for &byte in bytes {
-        encoded.push(ENCODE_CHARS[(byte >> 2) & 0b1111]);
-        encoded.push(ENCODE_CHARS[((byte << 4) & 0b11110000) | (byte >> 4)]);
-        encoded.push(ENCODE_CHARS[((byte << 2) & 0b11000000) | (byte >> 6)]);
-        encoded.push(ENCODE_CHARS[byte & 0b00111111]);
+        encoded.push_str(ENCODE_CHARS[(byte >> 2) & 0b1111]);
+        encoded.push_str(ENCODE_CHARS[((byte << 4) & 0b11110000) | (byte >> 4)]);
+        encoded.push_str(ENCODE_CHARS[((byte << 2) & 0b11000000) | (byte >> 6)]);
+        encoded.push_str(ENCODE_CHARS[byte & 0b00111111]);
     }
     encoded
 }
