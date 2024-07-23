b50591000644c1e8c67271ec16f90c056a0184d8
cursedcadaver <collinschacha@hotmail.com>
2024-07-23 19:45:23 +0300

    added ability to encode data using u8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
+const ENCODE_CHARS: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input = &args[1];
+    let bytes = input.as_bytes();
+    let encoded = encode_base64(bytes);
+    println!("{}", encoded);
+    println!("{:?}", bytes);
+}
+
+fn encode_base64(bytes: &[u8]) -> String {
+    let mut encoded = String::new();
+    for &byte in bytes {
+        encoded.push(ENCODE_CHARS[(byte >> 2) & 0b1111]);
+        encoded.push(ENCODE_CHARS[((byte << 4) & 0b11110000) | (byte >> 4)]);
+        encoded.push(ENCODE_CHARS[((byte << 2) & 0b11000000) | (byte >> 6)]);
+        encoded.push(ENCODE_CHARS[byte & 0b00111111]);
+    }
+    encoded
 }
